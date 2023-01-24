acad4e87f78e381971f475cb09426195558d2bf1
wajahatquazi <wajah@kloudone.com>
2023-01-24 06:14:08 +0530

    first screen

---
+++ src/create-Wallet.svelte
@@ -0,0 +1,52 @@
+<script>
+    import {
+      Button,
+      Link,
+      Loading,
+      TextArea,
+      TextInput,
+      Row,
+      Column,
+      FileUploader,
+    } from "carbon-components-svelte";
+
+    import Add from "carbon-icons-svelte"
+
+</script>
+
+
+<body>
+    <div class="createwallet-container">
+        <div class="title-container">
+            <h1>Wellcome to
+                Authnull Wallet
+            </h1>
+        </div>
+        <Row>
+         <Column>
+            <FileUploader
+            multiple
+            labelTitle="No,I Already have a Secret Recovery Phrase"
+            buttonLabel="Import Wallet"
+            labelDescription="Import your existing wallet using a secret recovery phrase"
+            accept={[".csv", ".jpeg"]}
+            status="complete"
+          />
+        </Column>
+  </Row>
+  <Row>
+    <Add />
+    <Column>
+    Yes,let's get set up!
+    </Column>
+    <Column>
+    This will create a new wallet and secret recovery phrase
+    </Column>
+    
+   <Button>Create a wallet</Button>
+  </Row>
+
+        
+    </div>
+    
+</body>
