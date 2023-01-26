10ab5e92534f22c5861ae1db075c01198efde4d2
wajahatquazi <wajah@kloudone.com>
2023-01-27 01:18:06 +0530

    receiving-credentials

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,16 @@
 <script>
    // import "./styles/index.scss"; 
-    import "./styles/list-of-Credentials.scss";
+   // import "./styles/list-of-Credentials.scss";
+   import "./styles/receiving-Credentials.scss";
     import CreateWallet from "./create-Wallet.svelte";
     import listofCredentials from "./list-of-Credentials.svelte"; 
     import "carbon-components-svelte/css/g100.css";
   import ListOfCredentials from "./list-of-Credentials.svelte";
+  import ReceivingCredentials from "./receiving-Credentials.svelte";
 </script>
 
 <div class="page-container">
-  <!--- <CreateWallet/> -->
-  <ListOfCredentials/> 
+  <!--- <CreateWallet/> 
+   <ListOfCredentials/> -->
+   <ReceivingCredentials/>
 </div>
