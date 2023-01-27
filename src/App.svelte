57de2887a4464896b947cfd1ce84c778b5ebf815
wajahatquazi <wajah@kloudone.com>
2023-01-27 09:47:50 +0530

    footer screen

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,9 @@
    // import "./styles/index.scss"; 
    //import "./styles/list-of-Credentials.scss";
   // import "./styles/receiving-Credentials.scss";
-  // import "./styles/activity.scss"
-     import "./styles/notifications.scss"
+  // import "./styles/activity.scss";
+   //  import "./styles/notifications.scss";
+   import "./styles/footer.scss";
     import CreateWallet from "./create-Wallet.svelte";
     import listofCredentials from "./list-of-Credentials.svelte"; 
     import "carbon-components-svelte/css/g100.css";
@@ -11,6 +12,7 @@
   import ReceivingCredentials from "./receiving-Credentials.svelte";
   import Activity from "./activity.svelte";
   import Notifications from "./notifications.svelte";
+  import Footer from "./footer.svelte";
 </script>
 
 <div class="page-container">
@@ -18,5 +20,6 @@
    <!-- <ListOfCredentials/>  -->
     <!-- <ReceivingCredentials/> -->
    <!-- <Activity/> -->
-   <Notifications/>
+   <!-- <Notifications/> -->
+   <Footer/>
 </div>
---
+++ src/footer.svelte
@@ -0,0 +1,57 @@
+<script>
+    import {
+         Button,
+         Link,
+         Loading,
+         TextArea,
+         TextInput,
+         Row,
+         Column,
+         Search,
+         Tab,
+         Tabs,
+         TabContent,
+       } from "carbon-components-svelte";
+
+       import {
+        Home,
+        Password,
+        Notification,
+        AppConnectivity,
+        CloudDownload,
+       }  from "carbon-icons-svelte"
+  
+   
+   </script>
+   
+   
+   
+       <div class="list-of-credentials">
+           
+                 <Row class="data-container">
+                 <Row class="data-tabs tab-background">
+                   <Column id="sub-tab1"><Home/></Column>
+                   <Column id="sub-tab1"><Password/></Column>
+                   <Column id="sub-tab1"><Notification/></Column>
+                   <Column id="sub-tab1"><AppConnectivity/></Column>
+                   <Column id="sub-tab1"><CloudDownload/></Column>
+                   
+                 </Row>
+               
+                
+                 
+                
+                   
+                 
+                 
+                 
+   
+            </Row>
+           
+               
+       
+           
+       </div>
+       
+   
+   
