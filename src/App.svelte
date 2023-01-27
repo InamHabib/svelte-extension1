a5d74437fefe0fc944c653e29bfb891c0899dad8
wajahatquazi <wajah@kloudone.com>
2023-01-27 06:33:05 +0530

    notifications screen

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,21 @@
    // import "./styles/index.scss"; 
    //import "./styles/list-of-Credentials.scss";
   // import "./styles/receiving-Credentials.scss";
-   import "./styles/activity.scss"
+  // import "./styles/activity.scss"
+     import "./styles/notifications.scss"
     import CreateWallet from "./create-Wallet.svelte";
     import listofCredentials from "./list-of-Credentials.svelte"; 
     import "carbon-components-svelte/css/g100.css";
   import ListOfCredentials from "./list-of-Credentials.svelte";
   import ReceivingCredentials from "./receiving-Credentials.svelte";
   import Activity from "./activity.svelte";
+  import Notifications from "./notifications.svelte";
 </script>
 
 <div class="page-container">
    <!-- <CreateWallet/>  -->
    <!-- <ListOfCredentials/>  -->
     <!-- <ReceivingCredentials/> -->
-   <Activity/>
+   <!-- <Activity/> -->
+   <Notifications/>
 </div>
---
+++ src/notifications.svelte
@@ -0,0 +1,91 @@
+<script>
+    import {
+         Button,
+         Link,
+         Loading,
+         TextArea,
+         TextInput,
+         Row,
+         Column,
+         Search,
+         Tab,
+         Tabs,
+         TabContent,
+       } from "carbon-components-svelte";
+   
+   </script>
+   
+   
+   
+       <div class="list-of-credentials">
+           <Row>
+               <Column class="title-container">
+               <h2>Notifications</h2>
+               </Column>
+           </Row>
+                 <Row class="data-container">
+                 <Row class="data-tabs tab-background">
+                   <Column id="sub-tab1"></Column>
+                   <Column id="sub-tab2"><h4>Server Group #1 </h4>
+                           <p>Requested access to Password</p>                
+                   </Column>
+                 </Row>
+                 <Row class="data-tabs tab-background">
+                   <Column id="sub-tab1"></Column>
+                   <Column id="sub-tab2"><h4>Server Group #1 </h4>
+                        
+                           <p>requested access to SSH Keys</p>                
+                   </Column>
+                 </Row>
+                 <Row class="data-tabs">
+                   <Column id="sub-tab1"></Column>
+                   <Column id="sub-tab2"><h4>Server Group #1 </h4>
+                           
+                           <p>OTP 45470</p>                
+                   </Column>
+                 </Row>
+                 <Row class="data-tabs">
+                   <Column id="sub-tab1"></Column>
+                   <Column id="sub-tab2"><h4>Server Group #1 </h4>
+                         
+                           <p>OTP 32784</p>                
+                   </Column>
+                 </Row>
+                 <Row class="data-tabs">
+                   <Column id="sub-tab1"></Column>
+                   <Column id="sub-tab2"><h4>Server Group #1 </h4>
+                           
+                           <p>requested access to SSH Keys</p>                
+                   </Column>
+                 </Row>
+                 <Row class="data-tabs">
+                   <Column id="sub-tab1"></Column>
+                   <Column id="sub-tab2"><h4>Server Group #1 </h4>
+                          
+                           <p>requested access to Password</p>                
+                   </Column>
+                 </Row>
+                 <Row class="data-tabs">
+                   <Column id="sub-tab1"></Column>
+                   <Column id="sub-tab2"><h4>Server Group #1</h4>
+                          
+                           <p>requested access to SSH Keys</p>                
+                   </Column>
+                 </Row>
+                 <Row class="data-tabs">
+                   <Column id="sub-tab1"></Column>
+                   <Column id="sub-tab2"><h4>Server Group #1 </h4>
+                           
+                           <p>OTP 00894</p>                
+                   </Column>
+                 </Row>
+   
+            </Row>
+           
+               
+       
+           
+       </div>
+       
+   
+   
